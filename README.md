dc6f4bb42721e6752a89825bd191f65b7c58897d
wo4mei3 <wo4mei3@gmail.com>
2023-10-16 12:05:08 +0900

    a commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # tyarith
+This prolog program runs the simply typed lambda calculus named tyarith which is introduced in TaPL in the big-step way.
+
+# Usage
+```
+$ swipl tyarith.pl
+Welcome to SWI-Prolog (threaded, 64 bits, version 8.4.2)
+SWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software.
+Please run ?- license. for legal details.
+
+For online help and background, visit https://www.swi-prolog.org
+For built-in help, use ?- help(Topic). or ?- apropos(Word).
+
+?- run(app(lambda(x,tynat,var(x)),succ(zero)),V,T).
+V = succ(0),
+T = tynat .
+
+```
+
+# Special Thanks
+The program is inspired by https://github.com/mitsuchi/copl-in-prolog 
--- tyarith.pl
+++ tyarith.pl
@@ -7,9 +7,31 @@ typing(G,succ(E),tynat) :- typing(G,E,tynat).
 typing(G,pred(E),tynat) :- typing(G,E,tynat).
 typing(G,iszero(E),tybool) :- typing(G,E,tynat).
 
-typing(G,var(X),T) :- first(bind(X,T),G).
-typing(G,lambda(bind(X,T1),E),tyfun(T1,T2)) :- typing([bind(X,T1)|G],E,T2).
+typing(G,var(X),T) :- lookupty(bindty(X,T),G).
+typing(G,lambda(X,T1,E),tyfun(T1,T2)) :- typing([bindty(X,T1)|G],E,T2).
 typing(G,app(E1,E2),T2) :- typing(G,E1,tyfun(T1,T2)),typing(G,E2,T1).
 
-first(bind(X,T),[bind(X1,T1)|_]) :- X = X1, T = T1.
-first(bind(X,T),[bind(X1,_)|Xs]) :- X \== X1, first(bind(X,T),Xs).
+lookupty(bindty(X,T),[bindty(X1,T1)|_]) :- X = X1, T = T1.
+lookupty(bindty(X,T),[bindty(X1,_)|Xs]) :- X \== X1, lookupty(bindty(X,T),Xs).
+
+eval(_,true,true).
+eval(_,false,false).
+eval(Env,if(E1,E2,_),V) :- eval(Env,E1,true), eval(Env,E2,V).
+eval(Env,if(E1,_,E3),V) :- eval(Env,E1,false), eval(Env,E3,V).
+
+eval(_,zero,0).
+eval(Env,succ(E),V) :- eval(Env,E,V1), V = succ(V1).
+eval(Env,pred(E),0) :- eval(Env,E,0).
+eval(Env,pred(E),V) :- eval(Env,E,succ(V)).
+eval(Env,iszero(E),true) :- eval(Env,E,0).
+eval(Env,iszero(E),false) :- eval(Env,E,succ(_)).
+
+eval(Env,var(X),V) :- lookupval(bindval(X,V),Env).
+eval(Env,lambda(X,T1,E),cls(Env,lambda(X,T1,E))).
+eval(Env,app(E1,E2),V) :- eval(Env,E1,cls(Env1,lambda(X,_,E))), eval(Env,E2,V1), eval([bindval(X,V1)|Env1],E,V).
+
+
+lookupval(bindval(X,V),[bindval(X1,V1)|_]) :- X = X1, V = V1.
+lookupval(bindval(X,V),[bindval(X1,_)|Xs]) :- X \== X1, lookupty(bindval(X,V),Xs).
+
+run(E,V,T) :- typing([],E,T), eval([],E,V).
